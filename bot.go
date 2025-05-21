598012f21168b4adbf6f7471be58dcca51723bf6
Anhgelus Morhtuuzh <anhgelus@anhgelus.world>
2025-05-21 12:10:30 +0200

    style(commands): rename commands.go into commands_creator.go

--- bot.go
+++ bot.go
@@ -3,11 +3,13 @@ package gokord
 import (
 	"errors"
 	"fmt"
+	"github.com/anhgelus/gokord/commands"
 	"github.com/anhgelus/gokord/utils"
 	"github.com/bwmarrin/discordgo"
 	"math/rand/v2"
 	"os"
 	"os/signal"
+	"slices"
 	"sync"
 	"syscall"
 	"time"
@@ -18,6 +20,8 @@ var (
 
 	ErrBadStatusType     = errors.New("bad status type, please use the constant")
 	ErrStatusUrlNotFound = errors.New("status url not found")
+
+	registeredCommands []*discordgo.ApplicationCommand
 )
 
 const (
@@ -136,4 +140,122 @@ func (b *Bot) Start() {
 	}
 
 	utils.SendSuccess("Bot shut down")
+}
+
+// updateCommands of the Bot
+func (b *Bot) updateCommands(client *discordgo.Session) {
+	// add ping command
+	b.Commands = append(
+		b.Commands,
+		NewCommand("ping", "Connect the ping of the bot").SetHandler(commands.Ping),
+	)
+	// removing old commands and skipping already registered commands
+	appID := client.State.Application.ID
+	cmdRegistered, err := client.ApplicationCommands(appID, "")
+	if err != nil {
+		utils.SendAlert("bot.go - Fetching slash commands", err.Error())
+		return
+	}
+	update := GetCommandsUpdate(b)
+	if update == nil {
+		utils.SendAlert("bot.go - Checking the update", "update is nil, check the log")
+		return
+	}
+	for _, d := range update.Removed {
+		id := slices.IndexFunc(cmdRegistered, func(e *discordgo.ApplicationCommand) bool {
+			return d == e.Name
+		})
+		if id == -1 {
+			utils.SendWarn("Command not registered cannot be deleted", "name", d)
+			continue
+		}
+		c := cmdRegistered[id]
+		err = client.ApplicationCommandDelete(appID, "", cmdRegistered[id].ApplicationID)
+		if err != nil {
+			utils.SendAlert(
+				"bot.go - Deleting slash command",
+				err.Error(),
+				"name",
+				c.Name,
+				"id",
+				c.ApplicationID,
+			)
+		}
+	}
+	// registering commands
+	guildID := ""
+	if Debug {
+		gs, err := client.UserGuilds(1, "", "", false)
+		if err != nil {
+			utils.SendAlert("bot.go - Fetching guilds for debug", err.Error())
+			return
+		} else {
+			guildID = gs[0].ID
+		}
+	}
+	var registeredCommands []*discordgo.ApplicationCommand
+	o := 0
+	for _, c := range append(update.Updated, update.Added...) {
+		o += 1
+		id := slices.IndexFunc(b.Commands, func(e *GeneralCommand) bool {
+			return c == e.Name
+		})
+		v := b.Commands[id]
+		cmd, err := client.ApplicationCommandCreate(client.State.User.ID, guildID, v.ToCmd().ApplicationCommand)
+		if err != nil {
+			utils.SendAlert("bot.go - Create application command", err.Error(), "name", c)
+			continue
+		}
+		registeredCommands = append(registeredCommands, cmd)
+		utils.SendSuccess(fmt.Sprintf("Command %s initialized", c))
+	}
+	l := len(registeredCommands)
+	if l != o {
+		utils.SendWarn(fmt.Sprintf("%d/%d commands has been created or updated", o, l))
+	} else {
+		utils.SendSuccess(fmt.Sprintf("%d/%d commands has been created or updated", o, l))
+	}
+	b.Version.UpdateBotVersion(b)
+}
+
+// setupCommandsHandlers of the Bot
+func (b *Bot) setupCommandsHandlers(s *discordgo.Session) {
+	if len(cmdMap) == 0 {
+		for _, c := range b.Commands {
+			utils.SendDebug("Setup handler", "command", c.Name)
+			if c.Subs != nil {
+				utils.SendDebug("Using general handler", "command", c.Name)
+				cmdMap[c.Name] = b.generalHandler
+			} else {
+				cmdMap[c.Name] = c.Handler
+			}
+		}
+		cmdMap["ping"] = commands.Ping
+	}
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := cmdMap[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+// unregisterGuildCommands used to unregister commands after closing the bot (Debug = true only)
+func (b *Bot) unregisterGuildCommands(s *discordgo.Session) {
+	if !Debug {
+		return
+	}
+	gs, err := s.UserGuilds(1, "", "", false)
+	if err != nil {
+		utils.SendAlert("bot.go - Fetching guilds for debug", err.Error())
+		return
+	}
+	guildID := gs[0].ID
+	for _, v := range registeredCommands {
+		err = s.ApplicationCommandDelete(s.State.User.ID, guildID, v.ID)
+		if err != nil {
+			utils.SendAlert("bot.go - Delete application command", err.Error())
+			continue
+		}
+	}
+	registeredCommands = []*discordgo.ApplicationCommand{}
 }
--- commands_creator.go
+++ commands_creator.go
@@ -1,11 +1,7 @@
 package gokord
 
 import (
-	"fmt"
-	"github.com/anhgelus/gokord/commands"
-	"github.com/anhgelus/gokord/utils"
 	"github.com/bwmarrin/discordgo"
-	"slices"
 )
 
 // Cmd is a discordgo.ApplicationCommand + its handler
@@ -58,8 +54,7 @@ type GCommandChoice struct {
 }
 
 var (
-	cmdMap             = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){}
-	registeredCommands []*discordgo.ApplicationCommand
+	cmdMap = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){}
 )
 
 // ToSimple turns SubCmd into a SimpleSubCmd
@@ -248,122 +243,4 @@ func (c *GCommandChoice) ToDiscordChoice() *discordgo.ApplicationCommandOptionCh
 		Name:  c.Name,
 		Value: c.Value,
 	}
-}
-
-// updateCommands of the Bot
-func (b *Bot) updateCommands(client *discordgo.Session) {
-	// add ping command
-	b.Commands = append(
-		b.Commands,
-		NewCommand("ping", "Connect the ping of the bot").SetHandler(commands.Ping),
-	)
-	// removing old commands and skipping already registered commands
-	appID := client.State.Application.ID
-	cmdRegistered, err := client.ApplicationCommands(appID, "")
-	if err != nil {
-		utils.SendAlert("commands.go - Fetching slash commands", err.Error())
-		return
-	}
-	update := GetCommandsUpdate(b)
-	if update == nil {
-		utils.SendAlert("commands.go - Checking the update", "update is nil, check the log")
-		return
-	}
-	for _, d := range update.Removed {
-		id := slices.IndexFunc(cmdRegistered, func(e *discordgo.ApplicationCommand) bool {
-			return d == e.Name
-		})
-		if id == -1 {
-			utils.SendWarn("Command not registered cannot be deleted", "name", d)
-			continue
-		}
-		c := cmdRegistered[id]
-		err = client.ApplicationCommandDelete(appID, "", cmdRegistered[id].ApplicationID)
-		if err != nil {
-			utils.SendAlert(
-				"commands.go - Deleting slash command",
-				err.Error(),
-				"name",
-				c.Name,
-				"id",
-				c.ApplicationID,
-			)
-		}
-	}
-	// registering commands
-	guildID := ""
-	if Debug {
-		gs, err := client.UserGuilds(1, "", "", false)
-		if err != nil {
-			utils.SendAlert("commands.go - Fetching guilds for debug", err.Error())
-			return
-		} else {
-			guildID = gs[0].ID
-		}
-	}
-	var registeredCommands []*discordgo.ApplicationCommand
-	o := 0
-	for _, c := range append(update.Updated, update.Added...) {
-		id := slices.IndexFunc(b.Commands, func(e *GeneralCommand) bool {
-			return c == e.Name
-		})
-		v := b.Commands[id]
-		cmd, err := client.ApplicationCommandCreate(client.State.User.ID, guildID, v.ToCmd().ApplicationCommand)
-		if err != nil {
-			utils.SendAlert("commands.go - Create application command", err.Error(), "name", c)
-			continue
-		}
-		registeredCommands = append(registeredCommands, cmd)
-		utils.SendSuccess(fmt.Sprintf("Command %s initialized", c))
-		o += 1
-	}
-	l := len(registeredCommands)
-	if l != o {
-		utils.SendWarn(fmt.Sprintf("%d/%d commands has been created or updated", o, l))
-	} else {
-		utils.SendSuccess(fmt.Sprintf("%d/%d commands has been created or updated", o, l))
-	}
-	b.Version.UpdateBotVersion(b)
-}
-
-// setupCommandsHandlers of the Bot
-func (b *Bot) setupCommandsHandlers(s *discordgo.Session) {
-	if len(cmdMap) == 0 {
-		for _, c := range b.Commands {
-			utils.SendDebug("Setup handler", "command", c.Name)
-			if c.Subs != nil {
-				utils.SendDebug("Using general handler", "command", c.Name)
-				cmdMap[c.Name] = b.generalHandler
-			} else {
-				cmdMap[c.Name] = c.Handler
-			}
-		}
-		cmdMap["ping"] = commands.Ping
-	}
-	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := cmdMap[i.ApplicationCommandData().Name]; ok {
-			h(s, i)
-		}
-	})
-}
-
-// unregisterGuildCommands used to unregister commands after closing the bot (Debug = true only)
-func (b *Bot) unregisterGuildCommands(s *discordgo.Session) {
-	if !Debug {
-		return
-	}
-	gs, err := s.UserGuilds(1, "", "", false)
-	if err != nil {
-		utils.SendAlert("commands.go - Fetching guilds for debug", err.Error())
-		return
-	}
-	guildID := gs[0].ID
-	for _, v := range registeredCommands {
-		err = s.ApplicationCommandDelete(s.State.User.ID, guildID, v.ID)
-		if err != nil {
-			utils.SendAlert("commands.go - Delete application command", err.Error())
-			continue
-		}
-	}
-	registeredCommands = []*discordgo.ApplicationCommand{}
 }
